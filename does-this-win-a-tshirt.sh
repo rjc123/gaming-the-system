6f4b0fd7c0f324a41593453b7670df13d9b80df9
rjc123 <robin.carswell@digital.cabinet-office.gov.uk>
2014-12-03 13:39:26 +0000

    Address comments raised in early development

--- does-this-win-a-tshirt.sh
+++ does-this-win-a-tshirt.sh
@@ -1,8 +1,5 @@
-touch thecount.txt
-
 for i in {1..50}
 do
 	echo This should be commit $i >> thecount.txt
-	git add thecount.txt
-	git commit -m "The count is now $i"
+	git commit -m "The count is now $i" thecount.txt
 done
